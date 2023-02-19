7f4a73b79ae39486846523b4fc6e790881fac105
Azarudeen <azarudeenasraffali@gmail.com>
2023-02-19 15:24:33 +0530

    project_setup

---
+++ src/main/java/com/funcoders/App.java
@@ -0,0 +1,13 @@
+package com.funcoders;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
